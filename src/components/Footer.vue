3cbf1974545e3c33651e11a35737c5e4f4a75f0e
Kristi <kristi.n.norris777@gmail.com>
2024-09-13 21:25:28 -0700

    adding links

---
+++ src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="content">
+    <a src="linkedin.com/kristinorris" >LinkedIn</a>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
