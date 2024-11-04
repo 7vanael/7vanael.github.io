d78e07f3054031a92d688a07ae761dc43992686c
Kristi <kristi.n.norris777@gmail.com>
2024-11-03 21:03:52 -0800

    added resume viewer

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,7 @@
     </div>
     <nav>
       <router-link to="/">Home</router-link>
-      <router-link to="/about">About</router-link>
+      <router-link to="/resume" target="_blank">Resume</router-link>
       <router-link to="/projects">Projects</router-link>
     </nav>
     <Footer />
--- src/views/ResumeView.vue
+++ src/views/ResumeView.vue
@@ -1,12 +1,23 @@
 <template>
     <div> 
         <h1>Resume for Kristi Norris</h1> 
+        <iframe
+        :src="url"
+        width="100%"
+        height="600px"
+        style="border: none;"
+        title="PDF Viewer"
+      ></iframe>
     </div>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+            url : '../../public/assets/documents/KristiNorrisResume.pdf'
+        }
+    }
 }
 </script>
 
